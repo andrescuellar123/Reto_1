e6801b3dc9aa5089d5013f181354d130d2eedea6
camilovivas <camilo-152000@hotmail.com>
2021-10-26 13:48:12 -0500

    create 3 fragments and bottomView in activity_main

---
+++ app/src/main/java/com/example/reto1/MapsFragment.java
@@ -0,0 +1,34 @@
+package com.example.reto1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MapsFragment extends Fragment {
+
+
+    public MapsFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static MapsFragment newInstance() {
+        MapsFragment fragment = new MapsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/reto1/PublicacionFragment.java
@@ -0,0 +1,34 @@
+package com.example.reto1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PublicacionFragment extends Fragment {
+
+    public PublicacionFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static PublicacionFragment newInstance() {
+        PublicacionFragment fragment = new PublicacionFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_publicacion, container, false);
+    }
+}
