c1e67ea69f45b485e4c512f4edadac93f30b8918
andrescuellar123 <47890673+andrescuellar123@users.noreply.github.com>
2021-11-03 22:14:11 -0500

    Doing post view , creating post view and fix the bar in the profile

---
+++ app/src/main/java/com/example/reto1/CreatePostFragment.java
@@ -0,0 +1,38 @@
+package com.example.reto1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CreatePostFragment extends Fragment {
+
+
+
+
+
+    public CreatePostFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static CreatePostFragment newInstance(String param1, String param2) {
+        CreatePostFragment fragment = new CreatePostFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_post, container, false);
+    }
+}
