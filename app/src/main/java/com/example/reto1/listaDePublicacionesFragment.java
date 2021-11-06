bafb19f934b8e1c78490a7f77a855e200e0e353c
andrescuellar123 <47890673+andrescuellar123@users.noreply.github.com>
2021-11-05 20:17:50 -0500

    view of selecting camera or galery and new class added

---
+++ app/src/main/java/com/example/reto1/listaDePublicacionesFragment.java
@@ -0,0 +1,39 @@
+package com.example.reto1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link listaDePublicacionesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class listaDePublicacionesFragment extends Fragment {
+
+
+
+    public listaDePublicacionesFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static listaDePublicacionesFragment newInstance(String param1, String param2) {
+        listaDePublicacionesFragment fragment = new listaDePublicacionesFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lista_de_publicaciones, container, false);
+    }
+}
