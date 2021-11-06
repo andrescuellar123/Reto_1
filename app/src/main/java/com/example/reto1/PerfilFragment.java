51e26ebc7c51361fc44b24d7eb3229d5c622c1ca
camilovivas <camilo-152000@hotmail.com>
2021-11-06 16:43:57 -0500

    change button to imageButton

--- app/src/main/java/com/example/reto1/PerfilFragment.java
+++ app/src/main/java/com/example/reto1/PerfilFragment.java
@@ -7,9 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PerfilFragment extends Fragment {
-
+    private ImageView imageBusinnes;
+    private TextView nameBusiness, descriptionBusiness, noImage;
+    private ImageButton editBusiness;
 
     public PerfilFragment() {
         // Required empty public constructor
@@ -27,7 +32,12 @@ public class PerfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+        imageBusinnes = view.findViewById(R.id.imageBusiness);
+        editBusiness = view.findViewById(R.id.editBusiness);
+        nameBusiness = view.findViewById(R.id.nameBusiness);
+        descriptionBusiness = view.findViewById(R.id.descripBusiness);
+        noImage = view.findViewById(R.id.noImage);
+        return view;
     }
 }
