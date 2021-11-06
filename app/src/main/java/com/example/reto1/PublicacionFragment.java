225359241ad25f0b9aa15850030f4e9acc00eb46
camilovivas <camilo-152000@hotmail.com>
2021-11-06 12:41:37 -0500

    create recyclerView

--- app/src/main/java/com/example/reto1/PublicacionFragment.java
+++ app/src/main/java/com/example/reto1/PublicacionFragment.java
@@ -3,6 +3,7 @@ package com.example.reto1;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,9 @@ import android.view.ViewGroup;
 
 public class PublicacionFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+
     public PublicacionFragment() {
-        // Required empty public constructor
     }
 
 
@@ -28,7 +30,9 @@ public class PublicacionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_publicacion, container, false);
+        recyclerView = view.findViewById(R.id.recycler);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_publicacion, container, false);
+        return view;
     }
 }
